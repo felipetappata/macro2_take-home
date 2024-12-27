a8ff6389e6b617630af9074b1dc512098696b37d
felipetappata <felipetappata@gmail.com>
2024-12-27 16:40:55 -0300

    Delete everything

--- code/swz/model/+model_4nk_yield/+sparse/+block/dynamic_5.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = dynamic_5(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(20)=y(19)-y(11);
-end
--- swz_code/SWZ23_ReStat/model/+model_4nk/+sparse/+block/static_4.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = static_4(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(8)=y(4)*params(6)*(params(18)-1)/(1-params(3));
-end
